ae5f3c667c9e24ad45e4707a5d491c891bd75f03
James-Randall-14 <jammerand14@gmail.com>
2023-07-02 13:27:08 -0700

    centralized deps is the dream

--- scripts/topside.bash
+++
@@ -1,8 +0,0 @@
-# This script launches the topside ROS2 instance.
-# Part of the CORE ROV system
-
-# This command sources our ROS2 overlay
-source /home/jhsrobo/corews/install/setup.bash
-
-# This command executes the ROS Launch File (which actually runs our code)
-ros2 launch core topside.yaml
