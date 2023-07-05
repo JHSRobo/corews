c0abef60269d4f6e78afb1650b355cbe41fea70c
James-Randall-14 <jammerand14@gmail.com>
2023-07-04 22:45:19 -0700

    finishing everything up before gazebo

--- src/core/core_lib/motion_simulator.py
+++ src/core/core_lib/motion_simulator.py
@@ -6,16 +6,14 @@ class MotionSimulator():
   def __init__(self, position):
     self.position = position
     self.velocity = 0
-    # Our motion topics run at around 30 hz
-    self.frequency = 30
     
   def update_position(self, velocity):
-    cutoff = 0.2 # Speed below which you can neglect velocity
+    cutoff = 0.05 # Speed below which you can neglect velocity
     if self.velocity <= cutoff:
       self.velocity = 0
     
     self.velocity = self.velocity * 0.8 + velocity * 0.2
-    self.position += self.velocity / self.frequency
+    self.position += self.velocity
 
   def get_position(self):
     return self.position
