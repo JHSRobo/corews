f1dab889eb9e78522079a4be3172f38c937c36d9
James-Randall-14 <jammerand14@gmail.com>
2023-10-15 17:41:06 -0700

    rov bringup and update scripts

---
+++ scripts/rov/rov_setup.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check sudo perms
+if [[ "$(id -u)" != 0 ]]
+  then echo "Please run as root"
+  exit
+fi
+
+adduser jhsrobo gpio
+
+# Edit Ubuntu Files
+rm -rf /home/jhsrobo/.bashrc
+rm -rf /home/jhsrobo/ROVMIND/tcu_repo_clone.sh
+rm -rf /home/jhsrobo/ROVMIND/tcu_bringup.sh
+cp /home/jhsrobo/ROVMIND/bashrc_bottom /home/jhsrobo/.bashrc
+
+# Add helpful stuff to ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >> /home/jhsrobo/.bashrc
+echo "source /home/jhsrobo/corews/install/setup.bash" >> /home/jhsrobo/.bashrc
+echo "alias bottomside=\"ros2 launch core bottomside.yaml\"" >> /home/jhsrobo/.bashrc
+
+# Add Network Shortcuts
+echo "192.168.1.100 topside" >> /etc/hosts
+echo "192.168.1.110 opside" >> /etc/hosts
+echo "192.168.1.111 bottomside" >> /etc/hosts
+
+# Install ROS
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+apt update
+apt install ros-humble-ros-base ros-dev-tools -y
+rosdep init
+sudo -u jhsrobo rosdep update
---
+++ scripts/rov/rov_update.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check sudo perms
+if [[ "$(id -u)" != 0 ]]
+  then echo "Please run as root"
+  exit
+fi
+
+# Update the corews
+git pull --hard
+cd /home/jhsrobo/corews/src
+
+# Remove old packages
+rm -rf thruster_interface gpio_control sensor_interface
+
+# Clone new packages
+git clone https://github.com/JHSRobo/thruster_interface
+git clone https://github.com/JHSRobo/gpio_control
+git clone https://github.com/JHSRobo/sensor_interface
+
+# Update dependencies
+sudo -u jhsrobo rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y --os=ubuntu:jammy
+
+# Compile and source
+cd /home/jhsrobo/corews
+colcon build
+source install/setup.bash
+
+# Give jhsrobo ownership of the workspace
+sudo chown jhsrobo: -R /home/jhsrobo/corews
