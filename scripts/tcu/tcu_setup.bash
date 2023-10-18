acc35825ca64e33d04bc8fb628a95b4b57a3e78f
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-10-17 23:19:55 -0700

    Update tcu_setup.bash

--- scripts/tcu/tcu_setup.bash
+++ scripts/tcu/tcu_setup.bash
@@ -24,7 +24,7 @@ echo "192.168.1.111 bottomside" >> /etc/hosts
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update
-apt install ros-humble-desktop ros-dev-tools -y
+apt install ros-humble-desktop ros-dev-tools ros-humble-ros-ign-bridge -y
 rosdep init
 sudo -u jhsrobo rosdep update
 
