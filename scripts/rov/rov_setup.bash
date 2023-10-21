7376e227ba6db6f6b24e9837fd415b215d7e0d13
James-Randall-14 <randallj24@student.jhs.net>
2023-10-21 18:26:39 +0000

    added dependencies for temp and humidity sensor

--- scripts/rov/rov_setup.bash
+++ scripts/rov/rov_setup.bash
@@ -28,7 +28,7 @@ rosdep init
 # You can find lists of all rosdep recognized packages here:
 # https://github.com/ros/rosdistro/tree/master/rosdep
 # If your package isn't in base.yaml or python.yaml, add it below.
-pip install adafruit-circuitpython-bme280 ninja meson libcamera-dev libcamera-tools
+pip install adafruit-circuitpython-bme280 adafruit-circuitpython-ahtx0 ninja meson libcamera-dev libcamera-tools
 
 # Call the update script
 bash /home/jhsrobo/corews/scripts/rov/rov_update.bash
