3d0db2b15f4812ba3d6e9e8b0dbf266bfd3eb9a0
James-Randall-14 <randallj24@student.jhs.net>
2023-10-23 03:59:24 +0000

    updated update script with command for installing non rosdep dependencies

--- scripts/rov/rov_update.bash
+++ scripts/rov/rov_update.bash
@@ -28,6 +28,12 @@ git clone https://github.com/JHSRobo/sensor_interface
 sudo -u jhsrobo rosdep update
 sudo -u jhsrobo rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y --os=ubuntu:jammy
 
+# Install Packages that are NOT RECOGNIZED BY ROSDEP
+# You can find lists of all rosdep recognized packages here:
+# https://github.com/ros/rosdistro/tree/master/rosdep
+# If your package isn't in base.yaml or python.yaml, add it below.
+pip install adafruit-circuitpython-bme280 adafruit-circuitpython-ahtx0 ninja meson libcamera-dev libcamera-tools
+
 # Give jhsrobo ownership of the workspace
 sudo chown jhsrobo: -R /home/jhsrobo/corews
 
