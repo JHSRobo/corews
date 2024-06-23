c58445854fbcdb00f836b7c7b8a44db12557c0ce
Jack-Frings <jackdfrings@protonmail.com>
2024-06-23 00:00:23 -0700

    ros2 jazzy version

--- scripts/tcu/tcu_setup.bash
+++ scripts/tcu/tcu_setup.bash
@@ -11,7 +11,7 @@ apt install gcc make perl curl pip wget gnupg -y
 pip install setuptools==58.2.0
 
 # Add helpful stuff to ~/.bashrc
-echo "source /opt/ros/humble/setup.bash" >> /home/jhsrobo/.bashrc
+echo "source /opt/ros/jazzy/setup.bash" >> /home/jhsrobo/.bashrc
 echo "source /home/jhsrobo/corews/install/setup.bash" >> /home/jhsrobo/.bashrc
 echo "alias topside=\"ros2 launch core topside.yaml\"" >> /home/jhsrobo/.bashrc
 
@@ -24,9 +24,9 @@ echo "192.168.1.111 bottomside" >> /etc/hosts
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update
-apt install ros-humble-desktop ros-dev-tools ros-humble-ros-ign-bridge -y
-rosdep init
-sudo -u jhsrobo rosdep update
+apt install ros-dev-tools
+apt upgrade
+apt install ros-jazzy-desktop
 
 # Install GitHub cli tools
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
@@ -35,12 +35,6 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt install gh -y
-
-# Install Gazebo Simulation
-wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
-apt update
-apt install ignition-fortress -y
 
 # Call the update script
 sudo bash /home/jhsrobo/corews/scripts/tcu/tcu_update.bash
