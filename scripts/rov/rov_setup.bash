a00c025c2509228417a52313f5cc45594452fad9
Jack Frings <119393931+Jack-Frings@users.noreply.github.com>
2024-09-04 18:43:35 -0700

    fixed uneccesary slowdown
    Was stopping the daemon before for ros thinking it was necessary but in reality the TCU will not need to restart the daemon

--- scripts/rov/rov_setup.bash
+++ scripts/rov/rov_setup.bash
@@ -20,9 +20,6 @@ echo "alias depinstall=\"rosdep install --from-paths /home/jhsrobo/corews/src --
 echo "alias sym=\"colcon build --symlink-install\""  >> /home/jhsrobo/.bashrc
 echo "export PYTHONWARNINGS=ignore" >> /home/jhsrobo/.bashrc
 
-# Fix werid issue with listing nodes and topics by restarting daemon
-echo "ros2 daemon stop" >> /home/jhsrobo/.bashrc
-
 # Add Network Shortcuts
 echo "192.168.1.100 topside" >> /etc/hosts
 echo "192.168.1.110 opside" >> /etc/hosts
