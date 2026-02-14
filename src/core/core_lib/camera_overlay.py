4dc353aed80a089ed9cabf51b96abeacc74b0340
mightylemongrass <157245866+mightylemongrass@users.noreply.github.com>
2026-02-14 11:33:23 -0800

    Updated crabs

--- src/core/core_lib/camera_overlay.py
+++ src/core/core_lib/camera_overlay.py
@@ -17,10 +17,37 @@ class HUD():
         self.left_align = int(self.display_width / 40)
         self.vertical_increment = int(self.display_height / 20)
         self.model = YOLO("/home/jhsrobo/corews/src/core/core_lib/best.pt")
+
         if torch.cuda.is_available():
             self.gpu = True
         else:
             self.gpu = False
+
+        self.f = 800
+        self.base_cube = np.float32([
+            [-1,-1,-1],
+            [ 1,-1,-1],
+            [ 1, 1,-1],
+            [-1, 1,-1],
+            [-1,-1, 1],
+            [ 1,-1, 1],
+            [ 1, 1, 1],
+            [-1, 1, 1]
+        ]) * 100
+
+        self.cube = self.base_cube * 0.3
+
+        self.edges = [
+            (0,1),(1,2),(2,3),(3,0),
+            (4,5),(5,6),(6,7),(7,4),
+            (0,4),(1,5),(2,6),(3,7)
+        ]
+
+        self.front_face = [4,5,6,7]
+
+        self.pos_x = resolution[0]//2
+        self.pos_y = resolution[1]//2
+
 
     # Overlays the current camera's index and nickname
     def add_camera_details(self, frame, index, nickname):
@@ -131,26 +158,86 @@ class HUD():
         return frame
 
     
-    def crab_model(self, frame):
+    def crab_model(self, frame): 
         if self.gpu:
-            results = self.model(frame, device=0, conf = 0.5)
+            results = self.model(frame, device=0, conf=0.5)
         else:
-            results = self.model(frame, device="cpu", conf = 0.5)
-        annotated_frame = results[0].plot()
-        predictions = results.pred[0]
-        
-        class_ids = predictions[:, -1].cpu().numpy()
-        confidence_scores = predictions[:, 4].cpu().numpy()
-        
-        target_class_id = 0
-        min_confidence = 0.5
-        
-        filtered_detections = predictions[(class_ids == target_class_id) & (confidence_scores >= min_confidence)]
-        num_filtered_detections = len(filtered_detections)
-        
+            results = self.model(frame, device="cpu", conf=0.5)
+
+        result = results[0]
+
+        if result.boxes is None or len(result.boxes) == 0:
+            num_filtered_detections = 0
+            annotated_frame = frame.copy()
+        else:
+            boxes = result.boxes
+            class_ids = boxes.cls
+            confs = boxes.conf
+
+            target_class_id = 0
+            min_confidence = 0.5
+
+            mask = (class_ids == target_class_id) & (confs >= min_confidence)
+
+            filtered_boxes = boxes[mask]
+
+            num_filtered_detections = mask.sum().item()
+
+            result.boxes = filtered_boxes
+            annotated_frame = result.plot()
+
+
         font_size = 0.6
-        position = (self.left_align, 7 * self.vertical_increment)
+        position = (self.left_align, 9 * self.vertical_increment)
         text = "Number of European Green Crabs: " + str(num_filtered_detections)
-        annotated_frame = self.add_text(frame, text, position, font_size)
-        
+
+        annotated_frame = self.add_text(annotated_frame, text, position, font_size)
+
         return annotated_frame
+         
+       
+
+    def overlay_3d(self, frame, roll, pitch):
+
+        R = self.rotation_matrix(roll, pitch)
+        rotated = (R @ self.cube.T).T
+
+        rotated += np.array([0, 0, 600])
+
+        projected = []
+        for point in rotated:
+            x = int((self.f * point[0] / point[2]) + self.pos_x)
+            y = int((self.f * point[1] / point[2]) + self.pos_y)
+            projected.append((x,y))
+
+        for edge in self.edges:
+            cv2.line(frame,
+                     projected[edge[0]],
+                     projected[edge[1]],
+                     (0,255,0), 2)
+
+        for i in range(4):
+            pt1 = projected[self.front_face[i]]
+            pt2 = projected[self.front_face[(i+1)%4]]
+            cv2.line(frame, pt1, pt2, (0,0,255), 3)
+
+        return frame
+
+
+    def rotation_matrix(self, roll, pitch):
+        r = np.deg2rad(roll)
+        p = np.deg2rad(pitch)
+
+        Rx = np.array([
+            [1,0,0],
+            [0,np.cos(r),-np.sin(r)],
+            [0,np.sin(r), np.cos(r)]
+        ])
+
+        Ry = np.array([
+            [ np.cos(p),0,np.sin(p)],
+            [0,1,0],
+            [-np.sin(p),0,np.cos(p)]
+        ])
+
+        return Ry @ Rx
