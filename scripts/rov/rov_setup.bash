99f87f256495d852fd8e0c9cdc94c9a18c93af2e
jack-frings <jackdfrings@protonmail.com>
2026-05-29 23:20:06 +0000

    added stereo install

--- scripts/rov/rov_setup.bash
+++ scripts/rov/rov_setup.bash
@@ -49,6 +49,12 @@ make
 cp ustreamer /usr/local/bin
 rm -rf ~/ustreamer
 
+# Install depthai for Stereovision
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
+pip install depthai
+
 # Give GPIO Permissions
 groupadd gpio
 usermod -aG gpio jhsrobo
